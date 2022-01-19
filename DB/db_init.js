16ffc46eb7a0d4c79b0ccdf6b77db2217016e3a4
Zouambia Sohaib <js_zouambia@esi.dz>
2022-01-19 15:22:01 +0100

    edited project config

--- DB/db_init.js
+++ DB/db_init.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
+import express from "express";
+import mongoose from "mongoose";
 
-const URI= "mongodb+srv://GDGCIT1:GDGCIT1@gdgcodeit1.zionq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const ConnectDB = async()=>{
-    await mongoose.connect(URI,);
+const URI= "mongodb+srv://GDGCIT1:GDGCIT1@gdgcodeit1.zionq.mongodb.net/myDatabase?retryWrites=true&w=majority";
+
+const ConnectDB = async () => {
+    await mongoose.connect(URI, {useUnifiedTopology: true, useNewUrlParser: true});
     console.log("Connection Done");
 }
 
-module.exports=ConnectDB;
+export default ConnectDB;
--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import ConnectDB from "./DB/db_init.js"
+
+import "dotenv/config";
+
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const ConnectDB=require('./DB/db_init');
 
-require("dotenv/config");
 
-app.use(cors());
+var corsOptions = {
+  origin: "http://localhost:5000"
+};
 
-//DB connection
-ConnectDB();
+app.use(cors(corsOptions));
 
-app.use(bodyParser.json());
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res) => {
   res.send("Welcome to the project");
 });
+
+//DB connection
+ConnectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
