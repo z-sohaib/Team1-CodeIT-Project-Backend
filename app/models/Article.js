a191048d80a58f05bde5b0ed2863602740e7a21a
ViNoS-ab <abdo.2004dz@gmail.com>
2022-01-20 15:07:51 +0100

    added article controllers

--- app/models/Article.js
+++ app/models/Article.js
@@ -1,35 +1,44 @@
-import mongoose from 'mongoose';
-const DOCUMENT_NAME = 'Article';
-const COLLECTION_NAME = 'articles';
+import mongoose from "mongoose";
+const DOCUMENT_NAME = "Article";
+const COLLECTION_NAME = "articles";
 
-
-export const articleSchema = new mongoose.Schema({ 
-    id : { 
-            type: mongoose.Schema.Types.String,
-            required: true, 
+export const articleSchema = new mongoose.Schema(
+  {
+    id: {
+      type: mongoose.Schema.Types.String,
+      required: true,
     },
-    Categorie: {                                
+    Categorie: {
+      type: mongoose.Schema.Types.String,
+      required: true,
+    },
+    Tags: [
+      {
         type: mongoose.Schema.Types.String,
         required: true,
-    },
-    Tags : [{ 
-        type: mongoose.Schema.Types.String,
-        required: true, 
-    }],
+      },
+    ],
     Title: {
-        type: mongoose.Schema.Types.String,
-        required: true,
+      type: mongoose.Schema.Types.String,
+      required: true,
     },
     Resume: {
-        type: mongoose.Schema.Types.String,
-        required: true,
+      type: mongoose.Schema.Types.String,
+      required: true,
     },
-    Picture : { 
-        type: mongoose.Schema.Types.String, 
-        required:true
+    Picture: {
+      type: mongoose.Schema.Types.String,
+      required: true,
     },
-}, {
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-export const ArticleModel = mongoose.model(DOCUMENT_NAME, articleSchema, COLLECTION_NAME)  
+const ArticleModel = mongoose.model(
+  DOCUMENT_NAME,
+  articleSchema,
+  COLLECTION_NAME
+);
+export default ArticleModel;
