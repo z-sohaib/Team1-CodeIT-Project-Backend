b39a7a2a5fc7c7357b0471ff9db856a775ade5ac
ViNoS-ab <abdo.2004dz@gmail.com>
2022-01-20 15:17:44 +0100

    added article controller

--- app/models/Article.js
+++ app/models/Article.js
@@ -2,57 +2,29 @@ import mongoose from "mongoose";
 const DOCUMENT_NAME = "Article";
 const COLLECTION_NAME = "articles";
 
-<<<<<<< HEAD
 export const articleSchema = new mongoose.Schema(
   {
-    id: {
+    categorie: {
       type: mongoose.Schema.Types.String,
       required: true,
     },
-    Categorie: {
-      type: mongoose.Schema.Types.String,
-      required: true,
-    },
-    Tags: [
+    tags: [
       {
         type: mongoose.Schema.Types.String,
         required: true,
       },
     ],
-    Title: {
-      type: mongoose.Schema.Types.String,
-      required: true,
-    },
-    Resume: {
-      type: mongoose.Schema.Types.String,
-      required: true,
-    },
-    Picture: {
-      type: mongoose.Schema.Types.String,
-      required: true,
-=======
-
-export const articleSchema = new mongoose.Schema({
-    categorie: {                                
-        type: mongoose.Schema.Types.String,
-        required: true,
-    },
-    tags : [{ 
-        type: mongoose.Schema.Types.String,
-        required: true, 
-    }],
     title: {
-        type: mongoose.Schema.Types.String,
-        required: true,
+      type: mongoose.Schema.Types.String,
+      required: true,
     },
     resume: {
-        type: mongoose.Schema.Types.String,
-        required: true,
+      type: mongoose.Schema.Types.String,
+      required: true,
     },
-    picture : { 
-        type: mongoose.Schema.Types.String, 
-        required:true
->>>>>>> b0ae3db4142b5fb438a8133e27e2c220395e87f6
+    picture: {
+      type: mongoose.Schema.Types.String,
+      required: true,
     },
   },
   {
